38a0f8f6fbb1efcd656666687172c4aa72846691
leesan <s24046@gsm.hs.kr>
2025-02-22 12:12:33 +0900

    Feat: FindIdPage UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import StartPage from "./pages/StartPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
+import FindIdPage from "./pages/FindIDPage";
 const App = () => {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<StartPage />} />
         <Route path="signin" element={<SigninPage />} />
         <Route path="signup" element={<SignupPage />} />
+        <Route path="findid" element={<FindIdPage />} />
       </Routes>
     </BrowserRouter>
   );
