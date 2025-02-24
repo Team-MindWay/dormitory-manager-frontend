189fdfc5b160a80fabc7393e71f46d37319db762
leesan <s24046@gsm.hs.kr>
2025-02-24 18:12:11 +0900

    refactor: useNavigate

--- src/components/StartBox.jsx
+++ src/components/StartBox.jsx
@@ -3,6 +3,9 @@ import "../styles/StartBox.css";
 import { useNavigate } from "react-router-dom";
 
 const StartBox = () => {
+  function onHandleClick() {
+    go("/signin");
+  }
   const go = useNavigate();
 
   return (
@@ -11,7 +14,9 @@ const StartBox = () => {
         <span className="logo-text">DOMA</span>를 시작해 볼까요?
       </div>
 
-      <button className="login-button">로그인</button>
+      <button className="login-button" onClick={onHandleClick}>
+        로그인
+      </button>
       <div className="signup-container">
         <p className="signup-text">아직 가입 전이면?</p>
         <p className="signup-link" onClick={() => go("/signup")}>
