6da2539e929d00c24264dd24cbdcf2f1e8e6733b
leesan <s24046@gsm.hs.kr>
2025-03-13 09:17:36 +0900

    fix: maxLength 수정

--- src/components/Common/NoticeItem.jsx
+++ src/components/Common/NoticeItem.jsx
@@ -3,30 +3,33 @@ import { marked } from "marked";
 import "../../styles/NoticeItem.css";
 
 const NoticeItem = ({ title, content }) => {
-    const maxLength = 150;
-    const [isExpanded, setIsExpanded] = useState(false);
+  const maxLength = 89;
+  const [isExpanded, setIsExpanded] = useState(false);
 
+  marked.setOptions({
+    gfm: true,
+    breaks: true,
+  });
 
-    marked.setOptions({
-        gfm: true,
-        breaks: true,
-    });
-
-    return (
-        <div className="notice-item" onClick={() => setIsExpanded(!isExpanded)}>
-            <div className="notice-content">
-                <div className="notice-title">{title}</div>
-                <div
-                    className="notice-text"
-                    dangerouslySetInnerHTML={{
-                        __html: isExpanded
-                            ? marked(content)
-                            : marked(content.length > maxLength ? content.slice(0, maxLength) + "..." : content),
-                    }}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="notice-item" onClick={() => setIsExpanded(!isExpanded)}>
+      <div className="notice-content">
+        <div className="notice-title">{title}</div>
+        <div
+          className="notice-text"
+          dangerouslySetInnerHTML={{
+            __html: isExpanded
+              ? marked(content)
+              : marked(
+                  content.length > maxLength
+                    ? content.slice(0, maxLength) + "..."
+                    : content
+                ),
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NoticeItem;
